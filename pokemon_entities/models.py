265fa8766fd1d271c821a0ea9df3ceea6531bb13
CaDiBob <93527781+CaDiBob@users.noreply.github.com>
2022-03-13 11:53:01 +0900

    Удалил из текстовых полей null=True

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,89 +2,30 @@ from django.db import models
 
 
 class Pokemon(models.Model):
-    title = models.CharField(
-        "Имя покемона на русском",
-        max_length=200
-    )
-    title_en = models.CharField(
-        "Имя покемона на английском",
-        max_length=200,
-        blank=True,
-        null=True
-    )
-    title_jp = models.CharField(
-        "Имя покемона на японском",
-        max_length=200,
-        blank=True,
-        null=True
-    )
-    image = models.ImageField(
-        "Изображение покемона",
-        blank=True,
-        null=True,
-    )
-    text = models.TextField(
-        "Описание покемона",
-        blank=True,
-        null=True
-    )
+    title = models.CharField("Имя покемона на русском", max_length=200)
+    title_en = models.CharField("Имя покемона на английском", max_length=200, blank=True)
+    title_jp = models.CharField("Имя покемона на японском", max_length=200, blank=True)
+    image = models.ImageField("Изображение покемона", blank=True, null=True)
+    text = models.TextField("Описание покемона", blank=True)
     previous_evolution = models.ForeignKey(
-        "self",
-        null=True,
-        blank=True,
-        on_delete=models.CASCADE,
-        verbose_name="Эволюция",
-        related_name="pokemon_set",
+        "self", null=True, blank=True, on_delete=models.CASCADE, verbose_name="Эволюция", related_name="pokemon_set"
     )
-
 
     def __str__(self):
         return self.title
 
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon,
-        verbose_name="Покемон",
-        on_delete=models.CASCADE,
-    )
+    pokemon = models.ForeignKey(Pokemon, verbose_name="Покемон", on_delete=models.CASCADE)
     lat = models.FloatField("Широта")
     lon = models.FloatField("Долгота")
-    appeared_at = models.DateTimeField(
-        "Время появления",
-        null=True,
-        blank=True,
-    )
-    disappeared_at = models.DateTimeField(
-        "Время исчезновения",
-        null=True,
-        blank=True,
-    )
-    level = models.IntegerField(
-        "Уровень",
-        blank=True,
-        null=True
-    )
-    health = models.IntegerField(
-        "Количество здоровья",
-        blank=True,
-        null=True
-    )
-    strength = models.IntegerField(
-        "Сила",
-        blank=True,
-        null=True,
-    )
-    defence = models.IntegerField(
-        "Защита",
-        blank=True,
-        null=True,
-    )
-    stamina = models.IntegerField(
-        "Выносливость",
-        blank=True,
-        null=True,
-    )
-
+    appeared_at = models.DateTimeField("Время появления", null=True, blank=True)
+    disappeared_at = models.DateTimeField("Время исчезновения", null=True, blank=True,)
+    level = models.IntegerField("Уровень", blank=True, null=True)
+    health = models.IntegerField("Количество здоровья", blank=True, null=True)
+    strength = models.IntegerField("Сила", blank=True, null=True)
+    defence = models.IntegerField("Защита", blank=True, null=True)
+    stamina = models.IntegerField("Выносливость", blank=True, null=True)
 
     def __str__(self):
         return self.pokemon.title
